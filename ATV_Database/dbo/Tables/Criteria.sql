f9255fd0ce9c56426bc5c90b640e3865b812a0a6
Tran Tien Dat <tiendatdeveloper1997@gmail.com>
2019-09-19 15:13:00 +0700

    [dattt] update project database

--- ATV_Database/dbo/Tables/Criteria.sql
+++ ATV_Database/dbo/Tables/Criteria.sql
@@ -1,6 +1,11 @@
 ﻿CREATE TABLE [dbo].[Criteria] (
-    [Id]          INT            IDENTITY (1, 1) NOT NULL,
-    [DisplayName] NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_Criteria] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [DisplayName]   NVARCHAR (MAX) NOT NULL,
+    [ArticleTypeId] INT            NOT NULL,
+    [Unit]          INT            NULL,
+    CONSTRAINT [PK_Criteria] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Criteria_ArticleType] FOREIGN KEY ([ArticleTypeId]) REFERENCES [dbo].[ArticleType] ([Id])
 );
+
+
 
